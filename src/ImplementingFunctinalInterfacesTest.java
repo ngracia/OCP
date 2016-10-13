4a3de061a9dd0bc2f1ca7b33389a84804cae022c
nestorDev <nestor.gracia@gmail.com>
2016-10-13 23:44:04 +0100

    tests

---
+++ src/ImplementingFunctinalInterfacesTest.java
@@ -0,0 +1,91 @@
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * Created by dev on 13/10/16.
+ */
+public class ImplementingFunctinalInterfacesTest {
+    public static void main(String args []){
+        ImplementingFunctinalInterfacesTest implementingFunctinalInterfacesTest = new ImplementingFunctinalInterfacesTest();
+        implementingFunctinalInterfacesTest.testBiConsumer();
+        implementingFunctinalInterfacesTest.testConsumer();
+        implementingFunctinalInterfacesTest.testSupplier();
+        implementingFunctinalInterfacesTest.testBiConsumer();
+    }
+
+    private void testSupplier(){
+        Supplier<List<String>> supplier1 = ArrayList::new;
+        Supplier<List<String>> supplier2 = () -> new ArrayList<>();
+        List<String> list1 = supplier1.get();
+        List<String> list2 = supplier2.get();
+
+        Consumer<String>  consumer1 = list1::add;
+        consumer1.accept("test1");
+        consumer1.accept("test2");
+        consumer1.accept("test3");
+        list1.forEach(System.out::println);
+
+        Consumer<String> print = System.out::print;
+        print.accept("\n");
+
+        Consumer<String>  consumer2 = x -> list2.add(x);
+        consumer2.accept("test4");
+        consumer2.accept("test5");
+        consumer2.accept("test6");
+        list2.forEach(System.out::println);
+
+        print.accept("\n");
+
+
+        //test Collection
+        Supplier<HashMap<String, String>> map = () -> new HashMap<>();
+        Supplier<List<String>> list = ArrayList::new;
+        Supplier<Map<Integer, Integer>> mapInteger = HashMap::new;
+
+    }
+
+    private void testConsumer(){
+
+        List<String> listString = Arrays.asList("Hola","Hello","Ciao");
+        listString.stream();
+        Consumer<String> consumerString = System.out::println;
+        consumerString.accept(listString.get(1));
+        consumerString.accept(listString.get(2));
+        listString.forEach(x-> x.contains("o"));
+
+        Consumer<String> print = System.out::print;
+        print.accept("\n");
+
+        List<Integer> listInteger = Arrays.asList(1,2,3);
+        listInteger.stream();
+        Consumer<Integer> consumerInteger = x -> System.out.println(x);
+        consumerInteger.accept(listInteger.size());
+        listInteger.forEach(x -> System.out.println(x == 1 ? 69 : 96));
+
+        print.accept("\n");
+    }
+
+    private void testBiConsumer(){
+        Supplier<HashMap<String, String>> map = () -> new HashMap<>();
+        HashMap hashMap = map.get();
+        BiConsumer<String, String> biConsumer = (x, y) -> hashMap.put(x,y);
+
+        biConsumer.accept("1","hola");
+        biConsumer.accept("2","hello");
+        biConsumer.accept("3","ciao");
+
+        hashMap.forEach((x,y) -> System.out.println("key: " + x + "\nValue: " + y ));
+    }
+
+    private void testPredicate(){
+        //Filtering or matching
+
+    }
+
+    private void testBiPredicate(){
+        //Filtering or matching
+
+    }
+}
