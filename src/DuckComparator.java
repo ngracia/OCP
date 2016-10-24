fc023c2ac8d0b80c4e12ce4a55c586c03487eb16
nestorDev <nestor.gracia@gmail.com>
2016-10-24 01:18:16 +0100

    Some more test.

---
+++ src/DuckComparator.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+
+/**
+ * Created by dev on 23/10/16.
+ */
+public class DuckComparator {
+}
---
+++ src/TestSquirrel.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dev on 23/10/16.
+ */
+public class TestSquirrel {
+    public static void main(String [] args){
+
+
+        List<Squirrel> squirrel = new ArrayList<>();
+
+        squirrel.add(new Squirrel("test1"));
+        squirrel.add(new Squirrel("test2"));
+        squirrel.add(new Squirrel("test3"));
+        squirrel.add(new Squirrel("test4"));
+
+
+    }
+}
